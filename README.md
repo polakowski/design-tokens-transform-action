8ccfad66173f439d5707d13721c3d586d0bcf54c
Marek Polakowski <9076315+polakowski@users.noreply.github.com>
2022-09-13 11:16:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GitHub Action - design-tokens-transform
+GitHub action that transforms your design tokens into CSS-like variables.
---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+export TEST='abc'
+
+echo 'Hello from entrypoint.sh'
+echo 'TEST:' $TEST
+echo 'INPUT_SOURCE_FILE:' $INPUT_SOURCE_FILE
